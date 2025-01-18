dba36a1067c1917da772e1dd8c855edcc5e20853
Tylor Trub <tylortrub@gmail.com>
2025-01-18 16:02:44 -0500

    Added work in progress UI for frmAppSettings.

--- GameVolumeHandler/frmAppSettings.Designer.cs
+++ GameVolumeHandler/frmAppSettings.Designer.cs
@@ -30,15 +30,33 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAppSettings));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
             this.panel1.Location = new System.Drawing.Point(1, 1);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(931, 517);
             this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(931, 517);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // frmAppSettings
             // 
@@ -56,6 +74,8 @@
             this.MinimizeBox = false;
             this.Name = "frmAppSettings";
             this.Text = "App Settings";
+            this.Load += new System.EventHandler(this.frmAppSettings_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -63,5 +83,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- GameVolumeHandler/frmAppSettings.cs
+++ GameVolumeHandler/frmAppSettings.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,55 @@ namespace GameVolumeHandler
 {
     public partial class frmAppSettings : Form
     {
+        string connectionString = @"Data Source=GameVolumeHandler.db;Version=3;";
+        // move ths into a app.config file lol
+
         public frmAppSettings()
         {
             InitializeComponent();
+        }
+
+        private async void frmAppSettings_Load(object sender, EventArgs e)
+        {
+            await LoadAppSettings();
+        }
+
+        private async Task LoadAppSettings()
+        {
+            //string[] runningProcesses = GetProcesses();
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                string selectQuery = "SELECT * FROM AppSettings;";
+                using (var command = new SQLiteCommand(selectQuery, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        int counter = 0;
+                        while (await reader.ReadAsync())
+                        {
+                            Label settingLabel = new Label();
+                            CheckBox settingLabelInput = new CheckBox();
+                            settingLabel.Text = reader["SettingName"].ToString();
+                            settingLabelInput.Text = Convert.ToBoolean(Convert.ToInt32(reader["IsActive"].ToString())) ? "Yes" : "No";
+                            settingLabelInput.Checked = Convert.ToBoolean(Convert.ToInt32(reader["IsActive"].ToString()));
+                            try
+                            {
+                                tableLayoutPanel1.Controls.Add(settingLabel, 0, counter);
+                                counter++;
+                                tableLayoutPanel1.Controls.Add(settingLabelInput, 1, counter);
+                                counter++;
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                                throw;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
